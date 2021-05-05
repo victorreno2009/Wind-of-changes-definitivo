d1210c1ff8e8f6dc0858d9d64f4618f6308e8681
Juvenal Amarante <juvenalamarante13@gmail.com>
2021-05-05 00:08:19 -0300

    Adicionando guarda da floresta

--- cenarios/Passagem_Floresta.gd
+++ cenarios/Passagem_Floresta.gd
@@ -2,6 +2,6 @@ extends Area2D
 
 
 func _on_Passagem_Floresta_body_entered(body):
-	get_tree().change_scene("res://cenarios/floresta.tscn")
+	get_tree().change_scene("res://cenarios/florestaEscura.tscn")
 
 
